be61446547e94ff76459932254b83122b62f8291
Sara <sarailic431@gmail.com>
2025-05-16 22:56:23 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,36 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([]);
+  const inputRef = useRef();
 
-  return <></>;
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const value = inputRef.current.value;
+
+    if (value === "") return;
+    setItems((prev) => {
+      return [...prev, value];
+    });
+  }
+
+  return (
+    <>
+      Search:
+      <input type="search" />
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New Item: <input ref={inputRef} type="text" />
+        <button type="submit">Add</button>
+      </form>
+      <h3>Items:</h3>
+      {items.map((item) => (
+        <div> {item} </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
